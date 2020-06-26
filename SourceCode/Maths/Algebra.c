e9e1b1ecff315b120785b8f1f02fa697b6931771
AMARJIT SINGH <amarjitmca04@gmail.com>
2020-06-26 11:01:52 +0530

    Added New Features 	Added mul Function 	Added quadratic and linear Equation Solvers

---
+++ SourceCode/Maths/Algebra.c
@@ -0,0 +1,10 @@
+
+void quadraticEquation(float a, float b, float c) {
+	// Calculate Roots of Quadratic Equation
+}
+
+void linearEquation(float a1, flaot b1, float c1, float a2, float b2, float c2) {
+	// Solve Simulaneous Linear Equaltions
+}
+
+
--- SourceCode/Maths/Arithmetic.c
+++ SourceCode/Maths/Arithmetic.c
@@ -7,3 +7,7 @@ int sub(int x, int y) {
 	return x - y;
 }
 
+int mul(int x, int y) {
+	return x * y;
+}
+
