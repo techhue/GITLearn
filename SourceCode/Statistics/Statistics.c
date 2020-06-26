a223855fadedd4e3cbf90b59759554ddf4241a88
AMARJIT SINGH <amarjitmca04@gmail.com>
2020-06-26 14:30:53 +0530

    Implemented Statistics Functionlity 	Design Choice: Design 2 	Implemented mean, mode and median

---
+++ SourceCode/Statistics/Statistics.c
@@ -0,0 +1,21 @@
+
+// Design 2
+void mean(float values[], float *result) {
+
+
+
+}
+
+void mode(float values[], float *result) {
+
+
+
+}
+
+void median(float values[], float *result) {
+
+
+
+}
+
+
